26ded041bcf88f3a62c98c0a21ee08e0c8a935bf
Vincent Reiley <ventsley@gmail.com>
2024-03-14 14:25:27 -0600

    modified for mongodb

--- web_files/index.js
+++ web_files/index.js
@@ -18,7 +18,28 @@ const userNames = client.db('users').collection('users');
     process.exit(1);
   });
 
+/* // Insert a document
+  const house = {
+    name: 'Beachfront views',
+    summary: 'From your bedroom to the beach, no shoes required',
+    property_type: 'Condo',
+    beds: 1,
+  };
+  await collection.insertOne(house);
 
+  // Query the documents
+  const query = { property_type: 'Condo', beds: { $lt: 2 } };
+  const options = {
+    sort: { score: -1 },
+    limit: 10,
+  };
+
+  const cursor = collection.find(query, options);
+  const rentals = await cursor.toArray();
+  rentals.forEach((i) => console.log(i));
+}
+
+main().catch(console.error); */
 
 const app = express();
 
