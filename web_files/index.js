db511b17c310b8b1fe8b69796302525e506e7996
Vincent Reiley <ventsley@gmail.com>
2024-03-22 13:01:36 -0600

    added user functions

--- web_files/index.js
+++ web_files/index.js
@@ -2,13 +2,17 @@ const config = require('./dbConfig.json');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const DB = require('./database.js');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('haircut');
-const PicCuts = client.db('haircut').collection('pics');
-const noPicCuts = client.db('haircut').collection('noPics');
-const userNames = client.db('users').collection('users');
+const db = client.db('lezi');
+// const PicCuts = client.db('haircut').collection('pics');
+// const noPicCuts = client.db('haircut').collection('noPics');
+const userNames = client.db('lezi').collection('users');
 
 (async function testConnection() {
     await client.connect();
@@ -19,27 +23,23 @@ const userNames = client.db('users').collection('users');
   });
 
 /* // Insert a document
-  const house = {
-    name: 'Beachfront views',
-    summary: 'From your bedroom to the beach, no shoes required',
-    property_type: 'Condo',
-    beds: 1,
+  const user = {
+    name: name,
+    password: passwordHash,
+    side_length: side,
+    top_length: top,
   };
   await collection.insertOne(house);
 
   // Query the documents
-  const query = { property_type: 'Condo', beds: { $lt: 2 } };
-  const options = {
-    sort: { score: -1 },
-    limit: 10,
+  const query = { name: 'Bob', password: input };
+  const options = {i
   };
 
-  const cursor = collection.find(query, options);
+  const cursor = collection.find(query);
   const rentals = await cursor.toArray();
   rentals.forEach((i) => console.log(i));
-}
-
-main().catch(console.error); */
+} */
 
 const app = express();
 
@@ -48,22 +48,77 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.use(express.json());
 app.use(bodyParser.text());
 app.use(express.static('public'));
-
+app.use(cookieParser());
+app.use(express.json());
 
 var apiRouter = express.Router();
 app.use('/api', apiRouter);
 
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await getUser(req.body.userName)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.userName, req.body.password);
+    setAuthCookie(res, user.token);
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+app.post('/auth/login', async (req, res) => {
+  const user = await getUser(req.body.userName);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+app.get('/user/me', async (req, res) => {
+  authToken = req.cookies['token'];
+  const user = await collection.findOne({ token: authToken });
+  if (user) {
+    res.send({ userName: user.userName });
+  }
+});
+
+function getUser(userName) {
+  return collection.findOne({ userName: userName });
+}
+
+async function createUser(userName, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = {
+    userName: userName,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await collection.insertOne(user);
+
+  return user;
+}
+
+function setAuthCookie(res, authToken) {
+  res.cookie('token', authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
 let cuts = [];
 apiRouter.get('/get-cuts', (_req, res) => {
-    res.send(cuts);
+  const cuts = DB.getHaircuts();
+  res.send(cuts);
 })
 
 apiRouter.post('/post-cut', (req, res) => {
-    cuts = updateCuts(req.body, cuts);
-    console.log(req.body);
-    console.log("1");
+    cuts = addHaircut(req.body);
     res.send(cuts);
-    //
 })
 
 app.use((_req, res) => {
