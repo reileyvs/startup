563ac20eabc27aa046d0579be71b335318a173d9
Vincent Reiley <ventsley@gmail.com>
2024-03-29 17:51:17 -0600

    implemented websocket for real-time notifications

--- web_files/public/notification.js
+++ web_files/public/notification.js
@@ -1,10 +1,38 @@
-setInterval(() => {
-    const int = Math.floor(Math.random() * 100);
-    const name = "User" + int;
-    const chatText = name + " submitted a haircut!";
-    localStorage.setItem("notif", chatText);
-  }, 10000);
+const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    socket.onmessage = async (message) => {
+      const thing = await message.data;
+      console.log(thing);
+      const msg = JSON.parse(message.data);
+        //Hank
+        displayMsg(msg.userName);
+    };
+
+    async function displayMsg(name) {
+      const chatText = { notif: `${name}` + " submitted a haircut!\n" };
+      var notifications = JSON.parse(localStorage.getItem("notif")) ?? [];
+      notifications.push(chatText);
+      if (notifications.length > 5) {
+        notifications.reverse();
+        notifications.length = 5;
+      }
+      notifications.forEach(element => {
+        console.log(element.notif);
+      });
+      localStorage.setItem("notif", JSON.stringify(notifications));
+    }
 
   function notifs() {
-    window.alert(localStorage.getItem("notif"));
+    const cont = JSON.parse(localStorage.getItem("notif"));
+    var stringy = "";
+    if(cont) {
+    cont.forEach(element => {
+      stringy += element.notif;
+    })
+  }
+  if (stringy) {
+    window.alert(stringy);
+  } else {
+    window.alert("No notifications");
+  }
   }
