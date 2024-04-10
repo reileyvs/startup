e91758f6a2ae3b38b31d5284a5ee427805780977
Vincent Reiley <vreiley@byu.edu>
2024-04-10 11:20:36 -0600

    Deleted unnecessary comments/Added Header and Footer components to App.jsx

--- web_files/React_Deliverable/src/App.jsx
+++ web_files/React_Deliverable/src/App.jsx
@@ -1,35 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
+  const header = Header();
+  const footer = Footer();
+  return (
+    <Header />,
+    <Footer />
+  )
+}
+
+function Header() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <head>
+      <meta charset="utf-8"></meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+      <title>Create your haircut</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"></link>
+      <link href="style.css"></link>
+    </head>
     </>
   )
+}
+
+function Main() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+function Footer() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <footer>
+        <span class="text-reset">Vincent Reiley</span>
+        <br />
+        <a href="https://github.com/reileyvs/startup">GitHub</a>
+      </footer>
+    </>
+  )
+}
+
+function NewElement() {
+  
 }
 
 export default App
