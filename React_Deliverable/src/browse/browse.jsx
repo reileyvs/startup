e05450f2695ebed1a9aa789d2167635603cec370
Vincent Reiley <vreiley@byu.edu>
2024-04-12 18:28:08 -0600

    browse page functional (I think)

--- React_Deliverable/src/browse/browse.jsx
+++ React_Deliverable/src/browse/browse.jsx
@@ -3,12 +3,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export function Browse() {
+    const [haircuts, setHaircuts] = React.useState([]);
+    React.useEffect(() => {
+        setHaircuts(localStorage.getItem("element"));
+        var arr = haircuts;
+        if(arr){
+            arr = arr.reverse();
+            if(arr.length > 12) {
+                arr.length = 12;
+            }
+            setHaircuts(arr);
+            arr.forEach(element => {
+                doc.insertAdjacentHTML('beforeend', element);
+            });
+        }
+
+    });
   return (
     <main className='container-fluid text-center'>
-      <div className="row" id="haircuts">Here are the haircuts
+      <div className="row" id="haircuts">{haircuts}
       </div>
       <hr />
     </main>
   );
 }
-
